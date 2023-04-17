cd4c8141930f11835f81523304c629299de57dd6
Marvin Arayan <marvin.arayan@vodafone.com>
2023-04-17 14:30:13 +0800

    fix api

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -52,15 +52,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://project-threejs-ai-1i5q.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
       const data = await response.json();
 
       handleDecals(type, `data:image/png;base64,${data.photo}`);
